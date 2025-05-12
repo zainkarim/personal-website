cf45ed04f1cacee08b16721cb106863f796acdda
Zain Karim <zain@zainkarim.com>
2025-05-11 20:10:49 -0500

    Added pixi-cli and dallas_skybot to home page

---
+++ main.js
@@ -0,0 +1,43 @@
+// JavaScript code to handle mobile menu toggle
+const menuButton = document.getElementById('mobile-menu-button')
+const menu = document.getElementById('nav-content')
+
+menuButton.addEventListener('click', () => {
+  if (
+    menuButton.classList.contains('bi-three-dots') &&
+    menu.style.display == 'none'
+  ) {
+    menu.style.display = 'block'
+    menuButton.classList.remove('bi-three-dots')
+    menuButton.classList.add('bi-x')
+  } else {
+    menu.style.display = 'none'
+    menuButton.classList.remove('bi-x')
+    menuButton.classList.add('bi-three-dots')
+  }
+})
+
+function refreshMenuCSS () {
+  if (window.innerWidth > 600) {
+    menu.style.display = 'block'
+    menuButton.classList.remove('bi-x')
+    menuButton.classList.add('bi-three-dots')
+  } else {
+    menu.style.display = 'none'
+  }
+}
+
+window.addEventListener('resize', refreshMenuCSS)
+
+const accordionButton = document.getElementById('accordion-button')
+const accordionLabel = document.getElementById('accordion-label')
+const honorsList = document.getElementById('select-honors')
+accordionButton.addEventListener('click', () => {
+  if (honorsList.style.display == 'none') {
+    honorsList.style.display = 'block'
+    accordionLabel.innerHTML = '- Select Exhibitions / Honors'
+  } else {
+    honorsList.style.display = 'none'
+    accordionLabel.innerHTML = '+ Select Exhibitions / Honors'
+  }
+})
